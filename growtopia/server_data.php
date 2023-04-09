23edfb70694f35f249f0722245f839b2f0cdf5b5
Nonzeee <rioqwerty120@gmail.com>
2023-04-10 02:03:03 +0700

    dwiadgwaiudgwa

---
+++ growtopia/server_data.php
@@ -0,0 +1,1 @@
+server|172.105.126.241 port|17091 type|1 #maint|Server is under maintenance. We will be back online shortly. Thank you for your patience! beta_server|beta.growtopiagame.com beta_port|27003 beta_type|1 meta|172.105.126.241 RTENDMARKERBS1001
